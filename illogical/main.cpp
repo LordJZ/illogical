f9cc4976fb44821f89f5fbfd447911019a4ceb79
LordJZ <jouterr@gmail.com>
2011-08-08 03:08:29 +0400

    Add "int(42) != int(42)"

--- illogical/main.cpp
+++ illogical/main.cpp
@@ -1,28 +1,48 @@
 
-#include <cstdio>
+#include <stdio.h>
 
-class A
+extern "C"
 {
-public:
-    int operator!()
+    class A
     {
-        //printf("op! called\n");
-        return 1;
-    }
+    public:
+        int operator!()
+        {
+            //printf("op! called\n");
+            return 1;
+        }
 
-    operator int()
+        operator int()
+        {
+            //printf("op int called\n");
+            return 1;
+        }
+    };
+
+    class B
     {
-        //printf("op int called\n");
-        return 1;
+    public:
+        B(__int64)
+        {
+        }
+
+        int operator!=(B other)
+        {
+            return 1;
+        }
+    };
+
+    #define ILLOGICAL(EXPR) { if (EXPR) printf("ILLOGICAL: true == " #EXPR "\n"); else printf("logical: false == " #EXPR "\n"); }
+
+    int main()
+    {
+    #define int B
+
+        A a;
+        ILLOGICAL(!a && a);
+        ILLOGICAL(int(42) != int(42));
+
+        getchar();
+        return 0;
     }
-};
-
-#define ILLOGICAL(EXPR) { if (EXPR) printf("ILLOGICAL: true == " #EXPR ); else printf("logical: false == " #EXPR); }
-
-int main()
-{
-    A a;
-    ILLOGICAL(!a && a);
-
-    getchar();
 }
